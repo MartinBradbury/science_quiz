226f7ab37009a9590eef157aff1edefaa1a1d7b0
MartinBradbury <>
2023-09-26 11:59:30 +0000

    added JS function to start quiz button to display game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,22 @@
 
 const start_btn = document.querySelector(".start_btn");
 const instructions_area = document.querySelector(".instructions_area");
-
-
+const exit_btn = document.querySelector(".exit_btn");
+const begin_btn = document.querySelector(".begin_btn");
+const game_area = document.querySelector(".game_area");
 
 // If the start button is clicked
 
 start_btn.onclick = () => {
     instructions_area.classList.add("activeInfo"); //Shows instructions on click
+};
+
+exit_btn.onclick = () => {
+    instructions_area.classList.remove("activeInfo");
+};
+
+begin_btn.onclick = () => {
+    instructions_area.classList.remove("activeInfo");
+    game_area.classList.add("activeInfo");
+
 };
