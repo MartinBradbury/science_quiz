da1331acfe98d31c254905d726b61d7166c1b572
MartinBradbury <>
2023-10-04 22:40:22 +0000

    creadted a questions.js and placed all questions in it and linked in HTML

---
+++ assets/js/questions.js
@@ -0,0 +1,206 @@
+// Creating an array and passing the question number, options and answer
+
+let questions = [
+    {
+        question: "How many Carbons in a Hexose sugar?",
+        answer: "6",
+        options: [
+            "5",
+            "6",
+            "7",
+            "8"
+        ]
+    },
+    {
+        question: "What is the name of the bonds that join Oxygen to the Hydrogen elements in a water molecule?",
+        answer: "Covalent",
+        options: [
+            "Strong Bonds",
+            "Hydrogen",
+            "Ionic",
+            "Covalent"
+        ]
+    },
+    {
+        question: " which part of the water molecule has a slightly negative charge?",
+        answer: "Oxygen",
+        options: [
+            "Hydrogen Bonds",
+            "Oxygen",
+            "Covalent Bonds",
+            "Hydrogen"
+        ]
+    },
+
+    {
+        question: "Why does water have surface tension?",
+        answer: "Hydrogen Bonds",
+        options: [
+            "Bond Angle",
+            "Hydrogen Bonds",
+            "Ionic Bonds",
+            "Covalent Bonds "
+        ]
+    },
+    {
+        question: "Which of these properties of water makes it a good environment for living organisms?",
+        answer: "All of the Above",
+        options: [
+            "High Latent Heat of Fusion",
+            "Universal Solvent",
+            "High Specific Heat Capacity",
+            "All of the Above"
+        ]
+    },
+    {
+        question: "What is the general formula for a carbohydrate?",
+        answer: "(CH2O)n",
+        options: [
+            "C6H12O6",
+            "(CH3)O6",
+            "n=Cn(H30)n ",
+            "(CH2O)n"
+        ]
+    },
+    {
+        question: "What are the 3 roles of Carbohydrates in organisms?",
+        answer: "Storage, Energy Source, Structure",
+        options: [
+            "Storage, Energy Source and Transport",
+            "Storage, Energy Source, Structure",
+            "Energy Source only ",
+            "Energy Source and Structure"
+        ]
+    },
+    {
+        question: "How many Carbon elements are there in a Triposephosphate?",
+        answer: "3",
+        options: [
+            "3",
+            "5 ",
+            "4 ",
+            "6"
+        ]
+    },
+    {
+        question: "Which structure contain Triosephosphate?",
+        answer: "ATP",
+        options: [
+            "Ribose",
+            "ATP",
+            "Amylase",
+            "rNADH"
+        ]
+    },
+    {
+        question: "How many carbons are there in a Pentoses Carbohydrate?",
+        answer: "5",
+        options: [
+            "4",
+            "5",
+            "6",
+            "7"
+        ]
+    },
+    {
+        question: "In which biological structure would you find a pentose carbohydrate?",
+        answer: "RNA",
+        options: [
+            "Glycerol",
+            "ATP",
+            "RNA",
+            "Glucose"
+        ]
+    },
+    {
+        question: "What is the structural difference between Ribose and Deoxyribose?",
+        answer: "No O on carbon 2",
+        options: [
+            "No OH on carbon 3",
+            "No H on carbon 3",
+            "No OH on carbon 1",
+            "No O on carbon 2"
+        ]
+    },
+    {
+        question: "What is the difference between Alpha and Beta Glucose?",
+        answer: "Alpha has H on top of C1",
+        options: [
+            "Alpha has H on top of C1",
+            "Alpha has OH on top of C1",
+            "Alpha has H on top",
+            "Alpha has OH on top"
+        ]
+    },
+    {
+        question: "What reagent is used to test for a reducing sugar?",
+        answer: "Benedicts",
+        options: [
+            "Iodine",
+            "Biuret",
+            "Benedicts",
+            "Methylene Blue"
+        ]
+    },
+    {
+        question: "What steps should you follow to test for a non-reducing sugar?",
+        answer: "Boil in HCL and neutralise in KOH",
+        options: [
+            "Boil in HCL and neutralise in H2SO4",
+            "Boil in KOH and neutralise in HCL",
+            "Boil in Ethanol and add water",
+            "Boil in HCL and neutralise in KOH"
+        ]
+    },
+    {
+        question: "What is the name of the bond between two Alpha Glucose Molecules?",
+        answer: "1-4 Glycosidic",
+        options: [
+            "1-4 Glycosidic",
+            "Ester",
+            "Peptide",
+            "Hydrogen"
+        ]
+    },
+    {
+        question: "What isomer of Glucose is used to form Starch?",
+        answer: "Alpha Glucose",
+        options: [
+            "Alpha Glucose",
+            "Beta Glucose",
+            "Triosephosphate",
+            "Ribose"
+        ]
+    },
+    {
+        question: "What is the name of the bond that connects Fatty acids to Glycerol?",
+        answer: "Ester",
+        options: [
+            "1-4 Glycosidic",
+            "Ester",
+            "Peptide",
+            "Hydrogen"
+        ]
+    },
+    {
+        question: "What molecule is added to glycerol to produce a Phospholipid?",
+        answer: "Phosphate",
+        options: [
+            "Lipid",
+            "Fatty Acid",
+            "Carbohydrate",
+            "Phosphate"
+        ]
+    },
+    {
+        question: "What does the term 'Hydrophilic' mean?",
+        answer: "Likes Water",
+        options: [
+            "Likes Water",
+            "Dissolves in Lipid",
+            "Dislikes Water",
+            "Surface Tension"
+        ]
+    },
+
+];
