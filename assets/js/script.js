4bcc98eec3136ed32b8cddc7c9ed8a3602666e9c
MartinBradbury <>
2023-10-07 13:04:13 +0000

    updated JShint warning to change .5 to '.5' in the random question sort

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ exit_btn.addEventListener("click", () => {
 });
 
 //shuffle array index questions so questions appear in a random order
-questions.sort(() => Math.random() - .5);
+questions.sort(() => Math.random() - '.5');
 
 begin_btn.addEventListener("click", () => {
     instructions_area.classList.remove("activeInfo");
