ef3795ec3769087e0d7ea3a3bdad571dc54edfcb
MartinBradbury <>
2023-11-26 12:50:41 +0000

    added film button function in javascript and linked to api category

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ const medium_btn = document.querySelector(".medium_btn");
 const hard_btn = document.querySelector(".hard_btn");
 const sci_btn = document.querySelector(".sci_btn");
 const games_btn = document.querySelector(".games_btn");
-
+const film_btn = document.querySelector(".film_btn");
 
 let questionElement = document.getElementById('question');
 let questionsList = [];
@@ -120,6 +120,12 @@ games_btn.addEventListener("click", () => {
     category_area.classList.remove("activeCategory");
     difficulty_area.classList.add("activeDifficulty");
     category = '15';
+});
+
+film_btn.addEventListener("click", () => {
+    category_area.classList.remove("activeCategory");
+    difficulty_area.classList.add("activeDifficulty");
+    category = '11';
 });
 
 // Reloads the window on restart test button click
